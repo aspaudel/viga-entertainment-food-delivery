608394c2660e07f565af8535fbc5b2dad69d9f8e
Adarsh Sharma <a.sharma30double@gmail.com>
2024-04-30 14:50:28 +0530

    require ssl for sequelize

--- db.js
+++ db.js
@@ -14,6 +14,12 @@ const port = process.env.DB_PORT;
 const sequelize = new Sequelize(database, user, password, {
   dialect: "postgres",
   host: process.env.DB_HOST,
+  ssl: true,
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
 });
 
 const testDbConnection = async () => {
